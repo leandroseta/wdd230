66312165c627fea5d164237d89a80e1f727f4bac
leandroseta <leandro.e.seta@gmail.com>
2023-01-06 01:49:56 -0300

    actualizacion

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1');
+const copyright = document.querySelector('#copyright');
+
+h1.textContent = 'Leandro Seta';
+copyright.textContent = `© ${new Date().getFullYear()}`;
+
+``
