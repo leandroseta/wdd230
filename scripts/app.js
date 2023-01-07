712ccd2ae5d8ab26236ff9f8b45d93d8eeae8570
leandroseta <leandro.e.seta@gmail.com>
2023-01-07 17:22:18 -0300

    update W01

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
+
+
 const h1 = document.querySelector('h1');
 const copyright = document.querySelector('#copyright');
+const update = document.querySelector('#update');
 
 h1.textContent = 'Leandro Seta';
 copyright.textContent = `© ${new Date().getFullYear()}`;
+update.textContent = `Last modified: ${new Date(document.lastModified)}`;
 
-``
