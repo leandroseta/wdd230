a8bddca4294fc4572d62143fa8581aa52e06345c
leandroseta <leandro.e.seta@gmail.com>
2023-02-03 01:56:27 -0300

    update

---
+++ lesson5/dom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+button.addEventListener("click", () => {
+    if(input.value !== "") {
+        let myName = input.value;
+     
+								const li = document.createElement("li");
+								const deleteBtn = document.createElement("button");
+					
+        li.textContent = myName;
+								deleteBtn.texContent = " ⚔ ";
+								deleteBtn.addEventListener("click", () => {
+										list.removeChild(li);
+								});
+					
+								li.appendChild(deleteBtn);
+        list.appendChild(li);
+        
+    }
+				input.Value = "";
+				input.focus();
+});
