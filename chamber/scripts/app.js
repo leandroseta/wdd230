e3336e300ef1ae0b922137528253bed0b4c8079c
leandroseta <leandro.e.seta@gmail.com>
2023-02-04 21:57:28 -0300

    update

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,5 +1,9 @@
-//date for header
 
+//modified update
+const update = document.querySelector('#update');
+update.textContent = `Last modified: ${new Date(document.lastModified).toLocaleString()}`;
+
+//header date
 const now = new Date();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
@@ -8,12 +12,16 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 document.getElementById('date').textContent = fulldateUK;
 document.getElementById('current-year').textContent = now.getFullYear();
 
-//hamburger 
-
+//hamburger menu
 function toggleNavMenu() {
     document.getElementById("headerNav").classList.toggle("open");
     document.getElementById("hamburger").classList.toggle("open");
 }
 const computer = document.getElementById("hamburger");
 computer.onclick = toggleNavMenu;
+
+
+
+
+
 
